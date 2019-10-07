e8df9d719c1fe48923cc9d8c0648e7739c3a0de6
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-07 11:53:11 -0500

    add faker dependency

--- src/main/java/MavenTest.java
+++ src/main/java/MavenTest.java
@@ -1,3 +1,4 @@
+import com.github.javafaker.Faker;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
@@ -5,20 +6,37 @@ import java.util.Scanner;
 public class MavenTest {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter Something: ");
+//        String userInput = sc.nextLine();
+//        System.out.println("You entered: " + userInput);
+//        if(StringUtils.isNumeric(userInput)) {
+//            System.out.println("That's a number");
+//        } else {
+//            System.out.println("That's not a number");
+//        }
+//
+//        System.out.println("Reverse case: " + StringUtils.reverse(userInput));
+//
+//        System.out.println("Flip case: " + StringUtils.swapCase(userInput));
 
-        System.out.println("Enter Something: ");
-        String userInput = sc.nextLine();
-        System.out.println("You entered: " + userInput);
-        if(StringUtils.isNumeric(userInput)) {
-            System.out.println("That's a number");
-        } else {
-            System.out.println("That's not a number");
-        }
+        Faker faker = new Faker();
 
-        System.out.println("Reverse case: " + StringUtils.reverse(userInput));
+        System.out.println(faker.princessBride().quote());
+        String name = faker.name().fullName(); // Miss Samanta Schmidt
+        String firstName = faker.name().firstName(); // Emory
+        String lastName = faker.name().lastName(); // Barton
 
-        System.out.println("Flip case: " + StringUtils.swapCase(userInput));
+        String streetAddress = faker.address().streetAddress(); // 60018 Sawayn Brooks Suite 449
+
+        System.out.println(name);
+
+        System.out.println(firstName);
+
+        System.out.println(lastName);
+
+        System.out.println(streetAddress);
 
 
     } //main()
